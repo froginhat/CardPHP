dedc46540370320b268eb4db28aa1bb6f9cc2fe8
froginhat <101974419+froginhat@users.noreply.github.com>
2024-02-06 11:44:00 +0300

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,17 @@
+
+<html>
+    <head>
+    <title>Веб-сайт</title>
+    </head>
+    <body>
+    
+    <?php
+    
+    echo "<p>Привет мир!</p>";
+    
+    
+    include  'index.php';   
+    ?>
+    </body>
+    </html>
+    
